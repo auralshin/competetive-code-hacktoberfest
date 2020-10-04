1845fbd3bac00fd921a047176b10d97888350274
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-10-04 23:57:00 +0530

    Add anotherway of bubble short in python

--- python/BubbleSort.py
+++ python/BubbleSort.py
@@ -13,3 +13,20 @@ arrays = [5,2,9,1,7,3,4,505,200]
 bubbleSort(arrays)
 #Printing array after bubble sort
 print(arrays)
+
+
+#Anotherway of implementing Bubble short inpython 
+
+def bubble_short(sequence):
+	indexing_length = len(sequence) - 1
+	shorted = False
+
+	while not shorted:
+		shorted = True
+		for i in range(indexing_length):
+			if sequence[i] > sequence[i+1]:
+				shorted = False
+				sequence[i],sequence[i+1] = sequence[i+1],sequence[i]
+	return sequence
+
+print(bubble_short([89,399,757,257,372,553,241,216,48,355,41,34,798,470,751]))
