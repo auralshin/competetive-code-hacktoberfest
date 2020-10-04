6fe24a13fd80eb466e022554d7a1ea9200457820
fharook shaik <fharookshaik.5@gmail.com>
2020-10-04 15:17:33 +0530

    Added a new sort technique 'Shell Sorting' in py.

---
+++ python/ShellSort.py
@@ -0,0 +1,29 @@
+def shellSort(input_list):
+    
+    gap = len(input_list) // 2
+    while gap > 0:
+
+        for i in range(gap, len(input_list)):
+            temp = input_list[i]
+            j = i
+# Sort the sub list for this gap
+
+            while j >= gap and input_list[j - gap] > temp:
+                input_list[j] = input_list[j - gap]
+                j = j-gap
+            input_list[j] = temp
+
+# Reduce the gap for the next element
+
+        gap = gap//2
+    
+    return input_list
+
+if __name__ == "__main__":
+    list = input().split()
+
+    for i in range(len(list)):
+        list[i] = int(list[i])
+
+    print(shellSort(list))
+    
