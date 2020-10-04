4aadbdedcd2bac7426f3dfdb8c523ca1b3cac913
fharook shaik <47080241+fharookshaik@users.noreply.github.com>
2020-10-04 15:20:47 +0530

    Update ShellSort.py

--- python/ShellSort.py
+++ python/ShellSort.py
@@ -6,15 +6,11 @@ def shellSort(input_list):
         for i in range(gap, len(input_list)):
             temp = input_list[i]
             j = i
-# Sort the sub list for this gap
-
+            
             while j >= gap and input_list[j - gap] > temp:
                 input_list[j] = input_list[j - gap]
                 j = j-gap
             input_list[j] = temp
-
-# Reduce the gap for the next element
-
         gap = gap//2
     
     return input_list
